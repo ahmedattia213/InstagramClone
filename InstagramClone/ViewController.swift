c93302a5a84eae8376f41d5568aedd851b3c47b2
ahmedattia213 <ahmedattia1927@gmail.com>
2019-07-28 01:16:27 +0200

    Initial commit project creation

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Ahmed Amr on 7/28/19.
+//  Copyright © 2019 Ahmed Amr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
